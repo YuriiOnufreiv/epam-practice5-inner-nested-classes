7c7e531f87497f4c353bf047ada305c90c6ec09c
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2016-11-30 20:31:27 +0200

    Added call to/from static/nonstatic fields/methods

--- src/ua/onufreiv/inner/MessengerFactory.java
+++ src/ua/onufreiv/inner/MessengerFactory.java
@@ -11,6 +11,9 @@ package ua.onufreiv.inner;
  * @author Yurii Onufreiv
  */
 public class MessengerFactory {
+    private static int staticField = 5;
+    private int nonStaticField = 10;
+
     /**
      * Creates the instance of {@link IMessenger} with the help of nested class
      * and returns its instance
@@ -18,7 +21,7 @@ public class MessengerFactory {
      * @return new instance of {@link IMessenger} class
      */
     public IMessenger getConsoleMessengerNested(String name) {
-        return new ConsoleMessengerNested(name);
+        return new ConsoleMessengerNested(name, this);
     }
 
     /**
@@ -32,7 +35,9 @@ public class MessengerFactory {
 
             @Override
             public void printMessage(String message) {
-                System.out.println(name + " (local) -> " + message);
+                System.out.println(name + " (local) -> " + message + " "
+                        + MessengerFactory.getFromStaticMethod() + " "
+                        + new MessengerFactory().getFromNonStaticMethod());
             }
         }
         return new ConsoleMessengerLocal();
@@ -59,14 +64,24 @@ public class MessengerFactory {
     private static class ConsoleMessengerNested implements IMessenger {
 
         private String messengerName;
+        private int sumOfFields;
 
-        private ConsoleMessengerNested(String name) {
+        private ConsoleMessengerNested(String name, MessengerFactory messengerFactory) {
             this.messengerName = name;
+            sumOfFields = MessengerFactory.staticField + messengerFactory.nonStaticField;
         }
 
         @Override
         public void printMessage(String message) {
-            System.out.println(messengerName + " (nested) -> " + message);
+            System.out.println(messengerName + " (nested) -> " + message + "; fields sum =  " + sumOfFields);
         }
+    }
+
+    public static String getFromStaticMethod() {
+        return "StaticFactoryMethod";
+    }
+
+    public String getFromNonStaticMethod() {
+        return "NonStaticFactoryMethod";
     }
 }
