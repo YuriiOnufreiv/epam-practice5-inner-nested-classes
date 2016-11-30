3d5e59a7b632ce49729d96fe726eac744abb824b
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2016-11-30 20:30:45 +0200

    Refactored position of inner classes

--- src/ua/onufreiv/airplane/Airplane.java
+++ src/ua/onufreiv/airplane/Airplane.java
@@ -8,44 +8,10 @@ package ua.onufreiv.airplane;
  * <p>1. {@code public class Airplane.AirplaneEngine implements IEngine}
  * <p>2. {@code public class Airplane.AirplaneEngine.Engine}
  *
- * @version 1.0
  * @author Yurii Onufreiv
+ * @version 1.0
  */
 public class Airplane {
-
-    public class AirplaneEngine implements IEngine {
-
-        private class Engine {
-            private EngineType engineType;
-
-            private Engine(EngineType type) {
-                this.engineType = type;
-            }
-
-            @Override
-            public String toString() {
-                return "Engine{" +
-                        "type='" + engineType +
-                        '}';
-            }
-        }
-
-        private Engine childEngine;
-
-        AirplaneEngine(EngineType engineType) {
-            this.childEngine = this.new Engine(engineType);
-        }
-
-        public void startEngine() {
-            System.out.println(" -> " + childEngine.engineType + " engine of "
-                    + Airplane.this + " is started");
-        }
-
-        public void stopEngine() {
-            System.out.println(" -> " + childEngine.engineType + " engine of "
-                    + Airplane.this + " engine is stopped");
-        }
-    }
 
     private String manufacturer;
     private String model;
@@ -80,5 +46,39 @@ public class Airplane {
     @Override
     public String toString() {
         return manufacturer + " " + model;
+    }
+
+    public class AirplaneEngine implements IEngine {
+
+        private Engine childEngine;
+
+        AirplaneEngine(EngineType engineType) {
+            this.childEngine = this.new Engine(engineType);
+        }
+
+        public void startEngine() {
+            System.out.println(" -> " + childEngine.engineType + " engine of "
+                    + Airplane.this + " is started");
+        }
+
+        public void stopEngine() {
+            System.out.println(" -> " + childEngine.engineType + " engine of "
+                    + Airplane.this + " engine is stopped");
+        }
+
+        private class Engine {
+            private EngineType engineType;
+
+            private Engine(EngineType type) {
+                this.engineType = type;
+            }
+
+            @Override
+            public String toString() {
+                return "Engine{" +
+                        "type='" + engineType +
+                        '}';
+            }
+        }
     }
 }
